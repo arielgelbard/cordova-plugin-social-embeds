c7b3c5ca1d10c2523f7328047a14283aa0d4b672
Jagraj Singh <jagraj.brar@gmail.com>
2017-02-02 15:59:27 -0500

    Updated android plugin

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -1,10 +1,15 @@
 package com.example.plugin;
 
 import org.apache.cordova.*;
+
+import android.util.Log;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+@SuppressLint("SetJavaScriptEnabled")
 public class Hello extends CordovaPlugin {
+    
+     CordovaWebView openWebView;
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
@@ -22,5 +27,25 @@ public class Hello extends CordovaPlugin {
             return false;
 
         }
+    }
+    
+    @Override
+    public void initialize (CordovaInterface cordova, CordovaWebView webView) {
+         openWebView = webView;
+    }
+
+    @Override
+    public boolean onOverrideUrlLoading(String url) {
+    	Log.d("OpenBlank", "onOverrideUrlLoading called with URL " + url);
+
+        try {
+            // This will execute openLinkInAppBrowser function defined in your javascript 
+            openWebView.loadUrl("javascript:openLinkInAppBrowser('"+ url +"')");
+        } catch (android.content.ActivityNotFoundException e) {
+                Log.d("OpenBlank", "OpenBlank: Error loading url "+url+":"+ e.toString());
+                return true;
+        }
+
+   		return true;
     }
 }
