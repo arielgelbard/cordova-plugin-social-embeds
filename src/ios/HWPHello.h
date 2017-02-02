1a06012f19a71ac2f959d4173c85901bacdaef50
Jagraj Singh <jsingh@jsingh.sjmedia.mag>
2017-02-02 15:11:04 -0500

    initial plugin setup for ios

--- src/ios/HWPHello.h
+++ src/ios/HWPHello.h
@@ -1,7 +1,8 @@
 #import <Cordova/CDV.h>
 
 @interface HWPHello : CDVPlugin
-
+@property (nonatomic, assign) BOOL flag;
+@property NSMutableArray *whiteList;
 - (void) greet:(CDVInvokedUrlCommand*)command;
 
 @end
--- src/ios/HWPHello.m
+++ src/ios/HWPHello.m
@@ -2,17 +2,50 @@
 
 @implementation HWPHello
 
+-(id)init {
+    if ( self = [super init] ) {
+        self.flag = false;
+    }
+    return self;
+}
+
 - (void)greet:(CDVInvokedUrlCommand*)command
 {
-
     NSString* name = [[command arguments] objectAtIndex:0];
-    NSString* msg = [NSString stringWithFormat: @"Hello, %@", name];
+
+    if([name isEqualToString:@""]){
+        NSLog(@"Plugin is disabled - - - - - - -");
+        self.flag = false;
+    } else {
+        NSLog(@"Plugin is enabled - - - - - - -");
+        self.flag = true;
+        self.whiteList = [[name componentsSeparatedByString: @","] mutableCopy];
+    }
 
     CDVPluginResult* result = [CDVPluginResult
                                resultWithStatus:CDVCommandStatus_OK
-                               messageAsString:msg];
-
+                               messageAsString:@"success"];
     [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+}
+
+- (BOOL)shouldOverrideLoadWithRequest:(NSURLRequest*)request navigationType:(UIWebViewNavigationType)navigationType
+{
+    BOOL allowNavigationsPass = true;
+
+    NSURL* url = [request URL];
+    if(self.flag) {
+        for (NSString *whiteListUrl in self.whiteList) {
+            if ([[url.absoluteString lowercaseString] containsString:[whiteListUrl lowercaseString]]) {
+                allowNavigationsPass = NO;
+                [self.commandDelegate evalJs:[NSString stringWithFormat:@"window.openLinkInAppBrowser('%@');", url.absoluteString]];
+                break;
+            }
+        }
+    } else {
+        allowNavigationsPass = YES;
+    }
+
+    return allowNavigationsPass;
 }
 
 @end
